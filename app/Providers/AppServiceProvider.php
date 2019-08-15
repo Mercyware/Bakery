8a5621aaace8f012ca546af332470dffa598ff95
Olaitan Adeboye <mercyware@gmail.com>
2019-08-15 15:48:30 +0100

    Refactored code, Add payment history and updating of suppliers record

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,8 +42,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        \URL::forceScheme('https');
-
+        if ( env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
 
