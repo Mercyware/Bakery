a87cf1a3ab47b7340029069ec78b719ed189eca2
Olaitan Adeboye <mercyware@gmail.com>
2019-08-15 17:32:02 +0100

    Re writing tests

---
+++ tests/Unit/SupplyServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SupplyServiceTest extends TestCase
+{
+    /**
+     *
+     */
+    public function test_that_createSupply_calls_createSupply_ofSupplyService_Successfully()
+    {
+
+    }
+
+
+}
